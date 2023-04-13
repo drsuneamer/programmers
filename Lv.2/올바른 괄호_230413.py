04a9b31d4d358568e9b3b757bfc6d2ad46252560
SY <97646668+drsuneamer@users.noreply.github.com>
2023-04-13 15:34:44 +0900

    [Lv2] 올바른 괄호 (python)

---
+++ Lv.2/올바른 괄호_230413.py
@@ -0,0 +1,27 @@
+def solution(s):
+    answer = True
+    
+    stack = []
+    
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        elif i == ')':
+            if len(stack) > 0 and stack[-1] == '(':
+                stack.pop(-1)
+            else:
+                answer = False
+                break
+    
+    if len(stack) > 0:
+        answer = False
+            
+    return answer
+  
+'''
+괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+
+"()()" 또는 "(())()" 는 올바른 괄호입니다.
+")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+'(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+'''
